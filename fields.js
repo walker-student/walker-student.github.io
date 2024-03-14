6eb5c65ee4757658cda3564de3a98226d4070242
WalkerG <Goldstein44@gmail.com>
2024-03-14 16:51:37 -0400

    removed feature

--- fields.js
+++ fields.js
@@ -12,18 +12,3 @@ document.getElementById('category').addEventListener('change', function() {
     }
 });
 
-// Dynamically add new rows
-
-$("#rowAdder").click(function () {
-    newRowAdd =
-        '<div id="row"> <div class="input-group m-3">' +
-        '<div class="input-group-prepend">' +
-        '<button class="btn btn-danger" id="DeleteRow" type="button">' +
-        '<i class="bi bi-trash"></i> Delete</button> </div>' +
-        '<input type="text" class="form-control m-input"> </div> </div>';
-
-    $('#newinput').append(newRowAdd);
-});
-$("body").on("click", "#DeleteRow", function () {
-    $(this).parents("#row").remove();
-});
