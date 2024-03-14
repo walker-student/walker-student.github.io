08f4341d6ce52c848df2e794aafe2db7211810d3
WalkerG <Goldstein44@gmail.com>
2024-03-14 12:26:11 -0400

    new submit screen

---
+++ sheets.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxvcalqAd3PPmMWDnI9U4Y-7mBCM654KasGmnAPGlmHysrLb4N1S78YrLsXuIOEh5w4tw/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
