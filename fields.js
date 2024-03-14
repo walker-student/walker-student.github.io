2324eaf597a8610d747297a92eed30120c00164d
WalkerG <Goldstein44@gmail.com>
2024-03-13 20:30:19 -0400

    text won't stay if category is changed

--- fields.js
+++ fields.js
@@ -3,8 +3,11 @@ document.getElementById('category').addEventListener('change', function() {
     if (category === 'drink') {
         document.getElementById('drinkFields').style.display = 'block';
         document.getElementById('foodFields').style.display = 'none';
+        document.getElementById('foodName').value = '';
     } else if (category === 'food') {
         document.getElementById('drinkFields').style.display = 'none';
         document.getElementById('foodFields').style.display = 'block';
+        document.getElementById('drinkName').value = '';
+        document.getElementById('size').value = '';
     }
 });
