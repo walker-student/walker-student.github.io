bf949ea696580811a10c93915e2b80df99cb798a
WalkerG <Goldstein44@gmail.com>
2024-03-22 14:52:55 -0400

    small fix

--- fields.js
+++ fields.js
@@ -1,8 +1,8 @@
 function incrementValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
+    var value = parseInt(document.getElementById('orderID').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
-    document.getElementById('number').value = value;
+    document.getElementById('orderID').value = value;
 }
 
 document.getElementById('category').addEventListener('change', function() {
